72d469ba883547933d36c0787d974c598d18a81f
Derek Cannistraro <cannistrarod@wit.edu>
2017-06-05 14:05:54 -0400

    Update useful_js.js

--- useful_js.js
+++ useful_js.js
@@ -1,6 +1,15 @@
-// Return URL Parameters by name
-// Ex: http://thing.com/?cat=cool getParameterByName('cat') returns 'cool'
+//Return URL Parameters by name
+//Ex: http://thing.com/?cat=cool getParameterByName('cat') returns 'cool'
 function getParameterByName(name) {
-        var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-        return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-    }
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+//Bootstrap Modal inside a modal fix
+$(document).on('show.bs.modal', '.modal', function () {
+    var zIndex = 1040 + (10 * $('.modal:visible').length);
+    $(this).css('z-index', zIndex);
+    setTimeout(function () {
+        $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+    }, 0);
+});
