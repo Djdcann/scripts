49573b1620b85a9bea80df158d1de45736ad312e
Derek Cannistraro <cannistrarod@wit.edu>
2017-07-05 14:38:08 -0400

    Update useful_js.js

--- useful_js.js
+++ useful_js.js
@@ -13,3 +13,40 @@ $(document).on('show.bs.modal', '.modal', function () {
         $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
     }, 0);
 });
+
+//copy text to clipboard
+function copyTextToClipboard(text) {
+  	    var textArea = document.createElement("textarea");
+        
+  	    textArea.style.position = 'fixed';
+  	    textArea.style.top = 0;
+  	    textArea.style.left = 0;
+        
+  	    textArea.style.width = '2em';
+  	    textArea.style.height = '2em';
+
+  	    textArea.style.padding = 0;
+        
+  	    textArea.style.border = 'none';
+  	    textArea.style.outline = 'none';
+  	    textArea.style.boxShadow = 'none';
+        
+  	    textArea.style.background = 'transparent';
+        
+        
+  	    textArea.value = text;
+        
+  	    document.body.appendChild(textArea);
+        
+  	    textArea.select();
+        
+  	    try {
+  		  var successful = document.execCommand('copy');
+  		  var msg = successful ? 'successful' : 'unsuccessful';
+  		  console.log('Copying text command was ' + msg);
+  	    } catch (err) {
+  		  console.log('Oops, unable to copy');
+  	    }
+        
+  	    document.body.removeChild(textArea);
+    }
