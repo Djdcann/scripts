b48e982fc1b53f46c0f3d19c541596111c004ebc
Derek Cannistraro <cannistrarod@wit.edu>
2017-10-12 16:21:21 -0400

    Create email.py

---
+++ email.py
@@ -0,0 +1,24 @@
+def send_email(user, pwd, recipient, subject, body):
+    import smtplib
+    
+    gmail_user = user
+    gmail_pwd = pwd
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        #server.ehlo()
+        #server.starttls()
+        server.login(gmail_user, gmail_pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print 'successfully sent the mail'
+    except Exception as e:
+        print e.message
+        print "failed to send mail"
